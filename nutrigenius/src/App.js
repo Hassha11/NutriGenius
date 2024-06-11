8e9711561c10482485ba05eb005cf02afce4c117
Hassha11 <madushanihashi@gmail.com>
2024-06-11 22:04:18 +0530

    update

---
+++ nutrigenius/src/App.js
@@ -0,0 +1,13 @@
+import LoginForm from './Components/LoginForm/LoginForm'
+import RegistrationForm from './Components/RegistrationForm/RegistrationForm';
+import BMIForm from './Components/BMIForm/BMIForm';
+
+function App() {
+  return (
+    <div>
+      <BMIForm/>
+    </div>
+  );
+}
+
+export default App;
