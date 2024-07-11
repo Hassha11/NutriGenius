a8d5771865a9435d1188bec55233bbb5d7e2f5ad
Hassha11 <madushanihashi@gmail.com>
2024-07-12 02:06:01 +0530

    Updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,32 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { Link, Outlet } from 'react-router-dom';
 import './Footer.css';
 
 const Footer = () => {
 
 return (
     <div className='wrapper-footer'>
-
+                <ul>
+                <h1>NutriGenius</h1><br></br>
+                <div className='flex-container'>
+                    <li>
+                        <Link to="/bmi">BMI Calculating</Link>
+                    </li>
+                    <li>
+                        <Link to="/">Analysis</Link>
+                    </li>
+                    <li>
+                        <Link to="/user">User Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/diet">Dietitian Recommendations</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    </div>
+                    </ul>
     </div>
 );
 };
