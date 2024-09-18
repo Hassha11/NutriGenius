31c70b1b2ebc803262c3555948cdac3ec46cbc82
Hassha11 <madushanihashi@gmail.com>
2024-09-18 22:36:57 +0530

    Updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
         <div className="layout-home">
         <Dashboard/>
         <Header/>
-            <h2 style={{ marginTop: '-60px', marginLeft: '-180px' }}>Welcome to NutriGenius...</h2>
+            <h1 style={{ marginTop: '-60px', marginLeft: '-180px', color:'black' }}>Welcome to NutriGenius...</h1><br></br>
             <p1><h3 style={{ top: '100px', marginLeft: '170px' }}>NutriGenius will give you the guidance you need to live a very healthy life.</h3></p1>
             <p2><h3 style={{ top: '100px', marginLeft: '170px' }}> A personalized diet plan is provoded here according to your BMI value.</h3></p2>
             <p3><h3 style={{ top: '100px', marginLeft: '170px' }}>So you can get your favourite foods very healthy regularly withoud any fear.</h3></p3>
